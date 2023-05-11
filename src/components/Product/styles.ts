1a5cfc9114dd5a0c989c02ac39cc4995b1ae2dac
samuelivanaskas <106975862+samuelivanaskas@users.noreply.github.com>
2023-05-11 10:23:15 -0300

    Add files via upload

---
+++ src/components/Product/styles.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+import { TagContainer } from '../Tag/styles'
+
+export const Card = styled.div`
+  background-color: ${cores.rosaclaro};
+  border-radius: 8px;
+  padding: 8px;
+  position: relative;
+
+  ${TagContainer} {
+    margin-right: 8px;
+  }
+`
+export const Botao = styled.button`
+  background-color: ${cores.rosa};
+  border: none;
+  width: 100%;
+  color: ${cores.preto};
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 16px;
+  cursor: pointer;
+  padding: 4px 0;
+`
+
+export const Titulo = styled.h3`
+  font-weight: bold;
+  font-size: 16px;
+  display: block;
+  margin-top: 16px;
+  margin-bottom: 8px;
+`
+
+export const Descricao = styled.p`
+  font-size: 14px;
+  line-height: 22px;
+  display: block;
+  margin-top: 16px;
+`
+
+export const Infos = styled.div`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+`
